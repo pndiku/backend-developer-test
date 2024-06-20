3b43e6c8031b2d807321d39f072d8470da9dfb9b
Peter C. Ndikuwera <pndiku@gmail.com>
2024-06-20 19:00:25 +0300

    Add caching of posts

--- app/api/v1/endpoints/post.py
+++ app/api/v1/endpoints/post.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 from typing import Any
 
 import fastapi_plugins
@@ -12,9 +14,32 @@ from app.api import deps
 router = APIRouter()
 
 
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "dict"):
+            return obj.dict()
+        elif isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat()
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+async def reCachePosts(db: Session, cache: aioredis.Redis, user_id: int):
+    posts = crud.post.get_for_user(db, user_id)
+
+    rows = [r.__dict__ for r in posts]
+
+    await cache.set(f"post-{user_id}", json.dumps(rows), ex=300)
+
+
 @router.post("/", response_model=schemas.PostResponse)
 async def create_post(
-    post_in: schemas.PostCreate, db: Session = Depends(deps.get_db), Authorize: AuthJWT = Depends()
+    post_in: schemas.PostCreate,
+    db: Session = Depends(deps.get_db),
+    Authorize: AuthJWT = Depends(),
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
 ) -> Any:
     """
     Create new post
@@ -25,6 +50,9 @@ async def create_post(
 
     r = crud.post.create(db, obj_in={"user_id": user_id, "text": post_in.text})
 
+    # re-cache posts
+    await reCachePosts(db, cache, Authorize.get_jwt_subject)
+
     return {"success": True, "data": r}
 
 
@@ -33,6 +61,7 @@ async def delete_post(
     id: int,
     db: Session = Depends(deps.get_db),
     Authorize: AuthJWT = Depends(),
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
 ) -> Any:
     """
     Delete post
@@ -49,6 +78,9 @@ async def delete_post(
 
     crud.post.remove(db, id=id)
 
+    # re-cache posts
+    await reCachePosts(db, cache, Authorize.get_jwt_subject)
+
     return {"success": True, "data": None}
 
 
@@ -56,6 +88,7 @@ async def delete_post(
 async def get_posts_for_user(
     db: Session = Depends(deps.get_db),
     Authorize: AuthJWT = Depends(),
+    cache: aioredis.Redis = Depends(fastapi_plugins.depends_redis),
 ) -> Any:
     """
     Get posts for a user
@@ -64,6 +97,13 @@ async def get_posts_for_user(
 
     user_id = Authorize.get_jwt_subject()
 
+    cachedRows = await cache.get(f"post-{user_id}")
+    if cachedRows:
+        return {"success": True, "data": json.loads(cachedRows)}
+
     posts = crud.post.get_for_user(db, user_id=user_id)
+
+    # re-cache posts
+    await reCachePosts(db, cache, Authorize.get_jwt_subject)
 
     return {"success": True, "data": posts}
